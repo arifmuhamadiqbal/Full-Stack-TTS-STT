251e638538b7e51d09f01452cc4924d21000d01c
arifmuhamadiqbal <arifmi2002@gmail.com>
2023-12-20 03:21:11 +0700

    revoke

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Main from "./Main.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
